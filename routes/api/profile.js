f7e34c263150eceb4f307a6a7757a4fc6b6447e9
Hemant Saini <hsainioff@gmail.com>
2020-08-16 10:46:15 +0530

    Get Profile By User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,4 +130,28 @@ router.get("/", async (req, res) => {
   }
 });
 
+//@route GET api/profile/user/:user_id
+//@desc Get profile by User Id.
+//@access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
