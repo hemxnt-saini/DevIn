8f9495cf6804daa2a40f29f6b7f05ff023e3f61c
Hemant Saini <hsainioff@gmail.com>
2020-08-16 10:33:11 +0530

    Get Request for all profiles

--- models/User.js
+++ models/User.js
@@ -27,4 +27,4 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,4 +115,19 @@ router.post(
   }
 );
 
+//@route GET api/profile
+//@desc Get All Profiles
+//@access Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
