8be5f18ef6746448df244afff08b75471f08dddc
Hemant Saini <hsainioff@gmail.com>
2020-08-18 20:26:56 +0530

    Deleting user educations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -314,4 +314,26 @@ router.put(
   }
 );
 
+//@route DELETE api/profile/education/:edu_id
+//@desc Delete profile education.
+//@access Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //get id of exp to be deleted
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
