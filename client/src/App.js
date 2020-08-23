77cf8ab90476f52e3f91ed5da432ca3105cd8433
Hemant Saini <hsainioff@gmail.com>
2020-08-23 12:16:38 +0530

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 import "./App.css";
 
 const App = () => {
-  return <Fragment>hi</Fragment>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
