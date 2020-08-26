92a13499d8e8ae3fdcd39d091318a961de20423d
Hemant Saini <hsainioff@gmail.com>
2020-08-26 17:59:07 +0530

    App.js with store

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-//Redux Entities
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
