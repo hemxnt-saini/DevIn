2f496164b28e60d939e373b403c406e971a2f15c
Hemant Saini <hsainioff@gmail.com>
2020-08-27 10:32:17 +0530

    UUID/V4 setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
@@ -16,6 +17,7 @@ const App = () => {
           <Navbar />
           <Route exact path='/' component={Landing} />
           <section className='container'>
+            <Alert />
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
