060682c5e0b9052b899917146d871a9c961c8365
Hemant Saini <hsainioff@gmail.com>
2020-08-28 20:49:36 +0530

    Private Route Created

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoutes";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
