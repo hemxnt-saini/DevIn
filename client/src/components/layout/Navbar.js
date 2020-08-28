03d8500e7f305ee802343886759ed8bc573990ad
Hemant Saini <hsainioff@gmail.com>
2020-08-28 21:23:52 +0530

    Dashboard Route Added in Nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,15 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          {" "}
+          <i className='fa fa-user' />{" "}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
-          <i className='fa fa-sign-out-alt'></i>{" "}
+          <i className='fa fa-sign-out-alt' />{" "}
           <span className='hide-sm'>Logout</span>
         </a>
       </li>
