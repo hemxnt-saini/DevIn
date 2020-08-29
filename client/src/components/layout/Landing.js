a27611b6c6790dc764c4a04d41fe21a86c3d37ba
Hemant Saini <hsainioff@gmail.com>
2020-08-29 22:23:19 +0530

    Landing page autherization

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <section className='landing'>
       <div className='dark-overlay'>
@@ -24,4 +29,12 @@ const Landing = () => {
     </section>
   );
 };
-export default Landing;
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+export default connect(mapStateToProps)(Landing);
