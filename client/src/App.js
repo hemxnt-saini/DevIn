ecc850a6d73a02eaaa036df42d77d79f7ed6d444
Hemant Saini <hsainioff@gmail.com>
2020-08-30 15:54:21 +0530

    Create Profile Route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoutes";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
@@ -34,6 +35,11 @@ const App = () => {
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
