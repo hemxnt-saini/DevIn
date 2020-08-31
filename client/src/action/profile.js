dc0aa5d80e12e8ff79a61f858c126913e7253bfe
Hemant Saini <hsainioff@gmail.com>
2020-08-31 17:41:23 +0530

    Action for Creating Profile

--- client/src/action/profile.js
+++ client/src/action/profile.js
@@ -19,3 +19,24 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or Update Profile
+
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {}
+};
