45d5c10f714ae1efb7cd33b89ef2480feb76396a
Hemant Saini <hsainioff@gmail.com>
2020-08-31 17:47:30 +0530

    Action for Creating Profile

--- client/src/action/profile.js
+++ client/src/action/profile.js
@@ -38,5 +38,21 @@ export const createProfile = (formData, history, edit = false) => async (
       type: GET_PROFILE,
       payload: res.data,
     });
-  } catch (err) {}
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "danger"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
 };
