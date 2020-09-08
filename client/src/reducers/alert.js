3c75017a4ef12f4b762aeee30f0ad7957ae944be
Hemant Saini <hsainioff@gmail.com>
2020-09-08 11:17:41 +0530

    Reducers

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,14 +1,15 @@
-import { SET_ALERT, REMOVE_ALERT } from "../action/types";
-const initalState = [];
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-export default function (state = initalState, action) {
+const initialState = [];
+
+export default function(state = initialState, action) {
   const { type, payload } = action;
-  //action.type//action.payload
+
   switch (type) {
     case SET_ALERT:
       return [...state, payload];
-    case REMOVE_ALERT: //remove a specific alert by its ID.
-      return state.filter((alert) => alert.id !== payload); // in this case of action payload is id !rem different different payload for different cases
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload);
     default:
       return state;
   }
