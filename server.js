2c24adf2807cc70dfcb42916dcb63149df031fb5
Hemant Saini <hsainioff@gmail.com>
2020-11-25 20:24:27 +0530

    Preparing for deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
+
 const app = express();
 
 connectDB();
@@ -7,14 +9,19 @@ connectDB();
 //Body Parser Middlware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Production static
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
